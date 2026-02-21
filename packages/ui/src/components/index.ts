2c8c0addc12b482b126ad0481a9ef2c84e42f5bd
Panawat (Wat) Atjanawat <patjanawat@gmail.com>
2026-02-22 01:16:26 +0700

    Clean

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,5 +1,1 @@
-export { Badge } from './Badge';
-export { Button } from './Button';
-export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './Card';
-export { Input } from './Input';
-export { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from './Table';
+export * from './app';
